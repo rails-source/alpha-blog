e694ea5c85e7e3d48febc01ba9eb6ec9e36ed0a3
rails source <railssource@gmail.com>
2016-02-09 18:14:14 +0000

    created users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119204839) do
+ActiveRecord::Schema.define(version: 20160209175848) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "Title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160119204839) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "changed_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
